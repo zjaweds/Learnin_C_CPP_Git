e6ab0a60bd843f80ecb2c7c7c9d5ef3234a96ba3
Jawed Alam <zjaweds@outlook.com>
2020-06-04 15:50:34 +0530

    Pattern Appended along with greetings

--- greet.cpp
+++ greet.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main(){
-	printf("\n\tHell's this World!\n");
+	printf("\n\tHell's this World!\n\t");
+	for(int i=0;i<6;i++){
+		for(int j=0;j<i;j++)
+			printf("*");
+		printf("\n\t");
+	}
 	return 0;
 }
